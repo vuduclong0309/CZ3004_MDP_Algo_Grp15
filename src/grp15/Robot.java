e81557aa89d9b92dccf526e7dfe53099c5c58562
vuduclong0309 <vuduclong0309@gmail.com>
2018-09-04 06:39:57 +0800

    added basic methods for object classes

--- src/grp15/Robot.java
+++ src/grp15/Robot.java
@@ -1,15 +1,77 @@
 package grp15;
 
+import static grp15.Direction.*;
+
 enum Direction {
     NORTH, SOUTH, EAST, WEST;
 }
 
 public class Robot {
     private int pos_x, pos_y;
-    private Direction direction = Direction.NORTH;
+    private Direction direction = NORTH;
 
     public Robot(int x, int y){
         this.pos_x = x;
         this.pos_y = y;
+    }
+
+    public void moveForward() {
+        switch (this.direction) {
+            case NORTH:
+                this.pos_y--;
+                break;
+            case SOUTH:
+                this.pos_y++;
+                break;
+            case EAST:
+                this.pos_x++;
+                break;
+            case WEST:
+                this.pos_x--;
+                break;
+        }
+    }
+
+    void turnLeft() {
+        switch (this.direction) {
+            case NORTH:
+                this.direction = WEST;
+                break;
+            case EAST:
+                this.direction = NORTH;
+                break;
+            case WEST:
+                this.direction = SOUTH;
+                break;
+            case SOUTH:
+                this.direction = EAST;
+                break;
+        }
+    }
+
+    public void turnRight() {
+        switch (this.direction) {
+            case NORTH:
+                this.direction = EAST;
+                break;
+            case EAST:
+                this.direction = SOUTH;
+                break;
+            case WEST:
+                this.direction = NORTH;
+                break;
+            case SOUTH:
+                this.direction = WEST;
+                break;
+        }
+    }
+
+    public void turnBack() {
+        this.turnLeft();
+        this.turnLeft();
+    }
+
+    public Direction getDirection() {
+        return this.direction;
     }
 }
