f81ea6dbf2b7071ba994f97e6da0bb90df4f5b77
vuduclong0309 <vuduclong0309@gmail.com>
2018-10-02 21:28:08 +0800

    module checkout

---
+++ algo_PC/src/grp15/object/CellColor.java
@@ -0,0 +1,32 @@
+package grp15.object;
+
+import java.awt.Color;
+
+public interface CellColor {
+    //blocked area
+    static final Color BLOCKED = Color.BLACK;
+
+    //free area
+    static final Color FREE = Color.WHITE;
+
+    //start zone
+    static final Color START = Color.GREEN;
+
+    //end zone
+    static final Color GOAL = Color.RED;
+
+    //unexplored area
+    static final Color UNEXPLORED = Color.DARK_GRAY;
+
+    //robot head
+    static final Color ROBOT_HEAD = Color.YELLOW;
+
+    //robot body
+    static final Color ROBOT_BODY = Color.GRAY;
+
+    //explore path
+    static final Color WAYPOINT = Color.MAGENTA;
+
+    //shortest path
+    static final Color SPATH = Color.CYAN;
+}
